24ffe3bdab2ea5988bf9656e411dd9c1f5261388
Aman Gupta <amg070@ucsd.edu>
2018-04-22 17:41:14 -0700

    map shows up

--- client/src/map.js
+++ client/src/map.js
@@ -4,6 +4,7 @@ console.log('here');
 
 mapboxgl.accessToken =
   'pk.eyJ1IjoiYW1hbmd1cHRhIiwiYSI6ImNqZzl2YnFyZzd1dmUycW1kb2RncmR1dHcifQ.6iKBun8VoyY2IUZDv7mZ9g';
+
 const map = new mapboxgl.Map({
   container: 'mapDiv',
   style: 'mapbox://styles/mapbox/streets-v10',
